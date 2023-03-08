4f04dfd87f98008f9727c256c42205fc6094d24d
Luis Alberto <luis.alberto@dspot.pl.com>
2023-03-08 17:00:08 -0500

    update auth

--- TestMid/settings.py
+++ TestMid/settings.py
@@ -93,6 +93,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
